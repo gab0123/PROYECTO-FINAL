6dd6f55f025058e0978704768a9b97b100a90108
gab0123 <gabo_1@outlook.es>
2016-07-24 15:24:18 -0500

    Funciòn menù
    nos ayudarà a elegir continentes

--- Registro.py
+++ Registro.py
@@ -3,23 +3,23 @@ import sys
 import time
 import sqlite3
 
-class Registro:
-    def __init__(self):
-        self.continente = ""
-        self.pais = ""
-        self.tecnico = ""
-        self.jugadores = []
-
-    def inscripcion(self):
+def menu_Continentes():
+    op = "0"
+    listCont = ["Africa","Asia","Europa","N/Centro America y Caribe","Oceania","Sudamerica"]
+    while(op == "0"):
+        print("\n\tCONTIENENTES")
+        print(" 1.- {}\n 2.- {}\n 3.- {}\n 4.- {}\n 5.- {}\n 6.- {}".format(listCont[0],listCont[1],listCont[2],listCont[3],listCont[4],listCont[5]))
+        op = str(input("\nIngrese un opcion: "))
+        if op not in ["1","2","3","4","5","6"]:
+            print("Opcion incorrecta, Vuelva a intentarlo!")
+            time.sleep(2)
+            op = "0"
         os.system("cls")
-        print("\n Ingrese los siguentes Datos:\n")
-        continente = str(input("Continente: "))
-        pais = str(input("Pais: "))
-        tecnico = str(input("Tecnico: "))
+    aux = int(op)-1
+    cont = listCont[aux]
+    return cont
 
-        con = sqlite3.connect("Registro.s3db")
-        cursor = con.cursor()
-        cursor.execute("insert into Registro (Continente, Pais, Tecnico) values ('"+continente+"','"+pais+"','"+tecnico+"')")
-        con.commit()
-        con.close()
-        print("Se registro con exito!")
+
+       
+                  
+               
