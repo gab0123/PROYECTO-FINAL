f8aeb080c1f34b40442737b48fa0ede28b592933
Rosángela Yllachura Arapa <ryllachura@gmail.com>
2016-07-28 19:02:54 -0500

    II

--- MUNDIAL.py
+++ MUNDIAL.py
@@ -15,3 +15,9 @@ class mundial:
         os.system("cls")
         Pais = str(input("Pais: "))
         Puntaje = int(input("Puntaje: "))
+
+        conexion = sqlite3.connect("mundial.s3db")
+        cursor = conexion.cursor()
+        cursor.execute("insert into mundial (Pais, Puntaje, Partidos_Ganados, Partidos_Empatados, Partidos_Perdidos  ) values ('" + Puntaje + "','"+Partidos_Ganados+"','" +Partidos_Empatados+ "','" +Partidos_Perdidos+ "')")
+        conexion.commit()
+        conexion.close(
