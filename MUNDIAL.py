8955fd334a4e9c9b8ebf78f0b3b5b04a3f9c20ec
Emiliana24 <mimi140711@gmail.com>
2016-07-29 12:07:49 -0500

    Update MUNDIAL.py

--- MUNDIAL.py
+++ MUNDIAL.py
@@ -20,4 +20,14 @@ class mundial:
         cursor = conexion.cursor()
         cursor.execute("insert into mundial (Pais, Puntaje, Partidos_Ganados, Partidos_Empatados, Partidos_Perdidos  ) values ('" + Puntaje + "','"+Partidos_Ganados+"','" +Partidos_Empatados+ "','" +Partidos_Perdidos+ "')")
         conexion.commit()
-        conexion.close(
+        conexion.close()
+        def tabla(self):
+        os.system("cls")
+        conexion = sqlite3.connect("mundial.s3db")
+        cursor = conexion.cursor()
+        cursor.execute("SELECT * from Grupos")
+        print("Pais\tPuntaje\t\tPG\t\tPE\t\tPP")
+        for i in cursor:
+            print("{}\t\t{}\t\t{}\t\t{}\t\t{}".format(i[1],i[2],i[3],i[4],i[5]))
+        conexion.commit()
+        conexion.close()
