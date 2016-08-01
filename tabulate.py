19d6c4ed17bac91b84a66fbd332d7dcfb7877531
gab0123 <gabo_1@outlook.es>
2016-07-31 23:40:33 -0500

    Update tabulate.py

--- tabulate.py
+++ tabulate.py
@@ -178,4 +178,185 @@ _table_formats = {"simple":
                               linebelowheader=Line("╞", "═", "╪", "╡"),
                               linebetweenrows=Line("├", "─", "┼", "┤"),
                               linebelow=Line("╘", "═", "╧", "╛"),
+                              headerrow=DataRow("│", "│", "│"),
+                              datarow=DataRow("│", "│", "│"),
+                              padding=0, with_header_hide=None),
+                  "pipe":
+                  TableFormat(lineabove=_pipe_line_with_colons,
+                              linebelowheader=_pipe_line_with_colons,
+                              linebetweenrows=None,
+                              linebelow=None,
+                              headerrow=DataRow("|", "|", "|"),
+                              datarow=DataRow("|", "|", "|"),
+                              padding=1,
+                              with_header_hide=["lineabove"]),
+                  "orgtbl":
+                  TableFormat(lineabove=None,
+                              linebelowheader=Line("|", "-", "+", "|"),
+                              linebetweenrows=None,
+                              linebelow=None,
+                              headerrow=DataRow("|", "|", "|"),
+                              datarow=DataRow("|", "|", "|"),
+                              padding=1, with_header_hide=None),
+                  "psql":
+                  TableFormat(lineabove=Line("+", "-", "+", "+"),
+                              linebelowheader=Line("|", "-", "+", "|"),
+                              linebetweenrows=None,
+                              linebelow=Line("+", "-", "+", "+"),
+                              headerrow=DataRow("|", "|", "|"),
+                              datarow=DataRow("|", "|", "|"),
+                              padding=1, with_header_hide=None),
+                  "rst":
+                  TableFormat(lineabove=Line("", "=", "  ", ""),
+                              linebelowheader=Line("", "=", "  ", ""),
+                              linebetweenrows=None,
+                              linebelow=Line("", "=", "  ", ""),
+                              headerrow=DataRow("", "  ", ""),
+                              datarow=DataRow("", "  ", ""),
+                              padding=0, with_header_hide=None),
+                  "mediawiki":
+                  TableFormat(lineabove=Line("{| class=\"wikitable\" style=\"text-align: left;\"",
+                                             "", "", "\n|+ <!-- caption -->\n|-"),
+                              linebelowheader=Line("|-", "", "", ""),
+                              linebetweenrows=Line("|-", "", "", ""),
+                              linebelow=Line("|}", "", "", ""),
+                              headerrow=partial(_mediawiki_row_with_attrs, "!"),
+                              datarow=partial(_mediawiki_row_with_attrs, "|"),
+                              padding=0, with_header_hide=None),
+                  "html":
+                  TableFormat(lineabove=Line("<table>", "", "", ""),
+                              linebelowheader=None,
+                              linebetweenrows=None,
+                              linebelow=Line("</table>", "", "", ""),
+                              headerrow=partial(_html_row_with_attrs, "th"),
+                              datarow=partial(_html_row_with_attrs, "td"),
+                              padding=0, with_header_hide=None),
+                  "latex":
+                  TableFormat(lineabove=_latex_line_begin_tabular,
+                              linebelowheader=Line("\\hline", "", "", ""),
+                              linebetweenrows=None,
+                              linebelow=Line("\\hline\n\\end{tabular}", "", "", ""),
+                              headerrow=_latex_row,
+                              datarow=_latex_row,
+                              padding=1, with_header_hide=None),
+                  "latex_booktabs":
+                  TableFormat(lineabove=partial(_latex_line_begin_tabular, booktabs=True),
+                              linebelowheader=Line("\\midrule", "", "", ""),
+                              linebetweenrows=None,
+                              linebelow=Line("\\bottomrule\n\\end{tabular}", "", "", ""),
+                              headerrow=_latex_row,
+                              datarow=_latex_row,
+                              padding=1, with_header_hide=None),
+                  "tsv":
+                  TableFormat(lineabove=None, linebelowheader=None,
+                              linebetweenrows=None, linebelow=None,
+                              headerrow=DataRow("", "\t", ""),
+                              datarow=DataRow("", "\t", ""),
+                              padding=0, with_header_hide=None)}
+
+
+tabulate_formats = list(sorted(_table_formats.keys()))
+
+
+_invisible_codes = re.compile(r"\x1b\[\d*m|\x1b\[\d*\;\d*\;\d*m")  # ANSI color codes
+_invisible_codes_bytes = re.compile(b"\x1b\[\d*m|\x1b\[\d*\;\d*\;\d*m")  # ANSI color codes
+
+
+def simple_separated_format(separator):
+    """Construct a simple TableFormat with columns separated by a separator.
+
+    >>> tsv = simple_separated_format("\\t") ; \
+        tabulate([["foo", 1], ["spam", 23]], tablefmt=tsv) == 'foo \\t 1\\nspam\\t23'
+    True
+
+    """
+    return TableFormat(None, None, None, None,
+                       headerrow=DataRow('', separator, ''),
+                       datarow=DataRow('', separator, ''),
+                       padding=0, with_header_hide=None)
+
+
+def _isconvertible(conv, string):
+    try:
+        n = conv(string)
+        return True
+    except (ValueError, TypeError):
+        return False
+
+
+def _isnumber(string):
+    """
+    >>> _isnumber("123.45")
+    True
+    >>> _isnumber("123")
+    True
+    >>> _isnumber("spam")
+    False
+    """
+    return _isconvertible(float, string)
+
+
+def _isint(string, inttype=int):
+    """
+    >>> _isint("123")
+    True
+    >>> _isint("123.45")
+    False
+    """
+    return type(string) is inttype or\
+           (isinstance(string, _binary_type) or isinstance(string, _text_type))\
+            and\
+            _isconvertible(inttype, string)
+
+
+def _type(string, has_invisible=True):
+    """The least generic type (type(None), int, float, str, unicode).
+
+    >>> _type(None) is type(None)
+    True
+    >>> _type("foo") is type("")
+    True
+    >>> _type("1") is type(1)
+    True
+    >>> _type('\x1b[31m42\x1b[0m') is type(42)
+    True
+    >>> _type('\x1b[31m42\x1b[0m') is type(42)
+    True
+
+    """
+
+    if has_invisible and \
+       (isinstance(string, _text_type) or isinstance(string, _binary_type)):
+        string = _strip_invisible(string)
+
+    if string is None:
+        return _none_type
+    elif hasattr(string, "isoformat"):  # datetime.datetime, date, and time
+        return _text_type
+    elif _isint(string):
+        return int
+    elif _isint(string, _long_type):
+        return _long_type
+    elif _isnumber(string):
+        return float
+    elif isinstance(string, _binary_type):
+        return _binary_type
+    else:
+        return _text_type
+
+
+def _afterpoint(string):
+    """Symbols after a decimal point, -1 if the string lacks the decimal point.
+
+    >>> _afterpoint("123.45")
+    2
+    >>> _afterpoint("1001")
+    -1
+    >>> _afterpoint("eggs")
+    -1
+    >>> _afterpoint("123e45")
+    2
+
+    """
+    if _isnumber(string):
 
